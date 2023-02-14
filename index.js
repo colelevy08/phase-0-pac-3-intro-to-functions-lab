66b54af9d1a744a2ef653263eedbde847f481d6e
colelevy <colelevy08@gmail.com>
2023-02-14 15:33:52 -0500

    commit tree b784eafdfcb309178efe795ca3c665c3415312a1

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string){
+    const Canthearyou = "I can\'t hear you!"
+    const Yes = "YES INDEED!"
+    const Love = "I would love to!"
+    if (string.toLowerCase(string) === string){
+        return Canthearyou
+    }
+    else if (string.toUpperCase(string) === string){
+        return Yes
+    }
+    else if ("Let\'s have dinner together!"){
+    return Love
+}
+}
